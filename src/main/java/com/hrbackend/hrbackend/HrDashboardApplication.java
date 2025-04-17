925f121010a8be409ba727cf825ceb5ee591d485
Venu <venu996366@gmail.com>
2025-04-17 16:15:24 +0530

    Updated application properties and main class configuration

--- src/main/java/com/hrbackend/hrbackend/HrDashboardApplication.java
+++ src/main/java/com/hrbackend/hrbackend/HrDashboardApplication.java
@@ -1,46 +1,4 @@
-package com.hrbackend.hrbackend;//package com.hrbackend.hrbackend;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class HrbackendApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(HrbackendApplication.class, args);
-//	}
-//
-//}
-
-
-
-//package com.hrbackend.hrbackend;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class HrDashboardApplication {
-//	public static void main(String[] args) {
-//		SpringApplication.run(HrDashboardApplication.class, args);
-//	}
-//}
-
-
-//package com.hrbackend.hrbackend;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-//
-//@SpringBootApplication
-//@EntityScan("com.hrbackend.hrbackend.entity")
-//public class HrDashboardApplication {
-//	public static void main(String[] args) {
-//		SpringApplication.run(HrDashboardApplication.class, args);
-//	}
-//}
-
+package com.hrbackend.hrbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
